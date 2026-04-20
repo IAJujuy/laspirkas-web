08c540da7e5e7951da962da78c2eb3a68507bffd
IAJujuy <jpserrarecuperar@gmail.com>
2026-04-20 08:42:10 -0300

    ticket oficial

--- src/main/java/com/evin/cap/medidas/repository/TicketMedidasRepository.java
+++
@@ -1,8 +0,0 @@
-package com.evin.cap.medidas.repository;
-
-import com.evin.cap.medidas.entity.TicketMedidas;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketMedidasRepository extends JpaRepository<TicketMedidas, Long> {
-    boolean existsByNumeroTicket(String numeroTicket);
-}
